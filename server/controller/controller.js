667fdf6e64ac1bbcaf4d36beb7d78811b4421dea
Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>
2022-09-30 12:30:03 +0530

    implemented api
    Signed-off-by: Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,23 +1,102 @@
-var Userdb = require('../model/model');
+var Userdb = require("../model/model");
 
 //cretae and save new user
 exports.create = (req, res) => {
-    
-}
+  //validate req
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+  //new user
+
+  const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+  // save user
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "some error while creating a create operation",
+      });
+    });
+};
 
 //retrieve and return all the users / retrive and return a single user
 exports.find = (req, res) => {
-    
-}
+  if (req.query.id) {
+    const id = req.query.id;
+    Userdb.findById(id)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({ message: "not found user with id " + id });
+        } else {
+          res.send(data);
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ message: "Error retrieving user with id" + id });
+      });
+  } else {
+    Userdb.find()
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || "error occured while retriving user information",
+        });
+      });
+  }
+};
 
 //update a new identified by userid
-exports.update = (req, res) =>
-{
-
-}
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Data to update cannot be empty" });
+  }
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot upadte user with ${id}.Maybe user not found!`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error update user information" });
+    });
+};
 
 //delete a user with specified user id in the request
-exports.delete = (req, res) =>
-{
-    
-}
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Userdb.findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .send({ message: `cannot delete with ${id}.Maybe id is wrong` });
+      } else {
+        res.send({
+          message: "user deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "could not delete user with id = " + id });
+    });
+};

ff1e7edec738383036114a62accb873ea24b7e4c
Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>
2022-09-30 11:00:09 +0530

    init
    Signed-off-by: Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require(`dotenv`).config();
+// const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
 
-const app = express();
-
-dotenv.config({ path: `config.env` });
+// dotenv.config({ path: `config` });
+// console.log(process.env.PORT);
 const PORT = process.env.PORT || 8080;
+// console.log(process.env.MONGO_URI);
+
+const connectDB = require("./server/database/connection");
+
+const app = express();
 
 //log requests
 app.use(morgan("tiny"));
+
+//mongodb connection
+connectDB();
 
 //parse requets to body-parser
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -24,10 +32,8 @@ app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-  //   res.send("crud application");
-});
+//load route
+app.use("/", require("./server/routes/router"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ server/controller/controller.js
@@ -0,0 +1,23 @@
+var Userdb = require('../model/model');
+
+//cretae and save new user
+exports.create = (req, res) => {
+    
+}
+
+//retrieve and return all the users / retrive and return a single user
+exports.find = (req, res) => {
+    
+}
+
+//update a new identified by userid
+exports.update = (req, res) =>
+{
+
+}
+
+//delete a user with specified user id in the request
+exports.delete = (req, res) =>
+{
+    
+}
